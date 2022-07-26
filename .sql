cd4950caf9e7f61deb9fd545259b6dcc25b2b1c3
Luan Severo <luansevero@icloud.com>
2022-07-26 20:18:38 -0300

    Fix id from bonus

--- .sql
+++ .sql
@@ -7,4 +7,4 @@ SELECT u.id, u.name, c.name as course, s.name as school, e."endDate" FROM users 
 --Questão 4
 SELECT u.id, u.name, r.name as role, c.name as company, e."startDate" FROM experiences e JOIN users u ON e."userId" = u.id JOIN roles r ON e."roleId" = r.id JOIN companies c ON e."companyId" = c.id WHERE e."endDate" IS null AND u.id = 50;
 --Questão Bônus:
-SELECT comp.id,s.name as school,c.name as course, comp.name as company, r.name as role FROM companies comp JOIN jobs j ON comp.id = j."companyId" JOIN roles r ON j."roleId" = r.id JOIN applicants a ON j.id = a."jobId" JOIN educations e ON a."userId" = e."userId" JOIN schools s ON e."schoolId" = s.id JOIN courses c ON e."courseId" = c.id WHERE comp.id = 10 AND j.active = true;
+SELECT s.id,s.name as school,c.name as course, comp.name as company, r.name as role FROM companies comp JOIN jobs j ON comp.id = j."companyId" JOIN roles r ON j."roleId" = r.id JOIN applicants a ON j.id = a."jobId" JOIN educations e ON a."userId" = e."userId" JOIN schools s ON e."schoolId" = s.id JOIN courses c ON e."courseId" = c.id WHERE comp.id = 10 AND j.active = true;
